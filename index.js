8b8d5a70039215ab96efd4b1ad168520a63cd2b9
Dayo Adebanjo <dahyor@github.com>
2021-05-16 20:40:25 +0100

    Added Billing and client side app

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-require("./models/user");
-require("./services/passport");
-const PORT = process.env.PORT || 5000;
+const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+require('./models/user')
+require('./services/passport')
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
 
-const app = express();
+const app = express()
 
+app.use(express.json())
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
-  })
-);
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey],
+	})
+)
 app.use(passport.initialize())
 app.use(passport.session())
 
-require("./routes/authRoutes")(app);
-app.listen(PORT);
+require('./routes/authRoutes')(app)
+require('./routes/billingRoutes')(app)
+
+if(process.env.NODE_ENV === 'production'){
+	// serve up production assets
+	app.use(express.static('client/build'))
+	//serve up index.html
+	const path = require('path')
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'client','build', 'index.html')))
+}
+app.listen(PORT)
--- services/passport.js
+++ services/passport.js
@@ -1,33 +1,29 @@
-const passport = require("passport");
-const mongoose = require("mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys");
-const User = mongoose.model("users");
+const passport = require('passport')
+const mongoose = require('mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('../config/keys')
+const User = mongoose.model('users')
 passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+	done(null, user.id)
+})
 passport.deserializeUser((user_id, done) => {
-  User.findById(user_id).then((user) => done(null, user));
-});
+	User.findById(user_id).then(user => done(null, user))
+})
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          // user record existss
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
-    }
-  )
-);
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback',
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id })
+			if (existingUser) {
+				return done(null, existingUser)
+			}
+			const user = await new User({ googleId: profile.id }).save()
+			done(null, user)
+		}
+	)
+)
